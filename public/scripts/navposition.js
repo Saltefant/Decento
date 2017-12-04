dadc1cd4e15ea42e527173a5c9431c025acd5276
Saltefant <LSkovlund@hotmail.com>
2017-12-04 21:26:21 +0100

    j

--- public/scripts/navposition.js
+++ public/scripts/navposition.js
@@ -33,7 +33,7 @@ function ResponsiveFunction() {
 
 //Login modal
 // Get the modal
-var modal = document.getElementById('id01');
+var modal = document.getElementById('modalId');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
